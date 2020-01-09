c5cfd3659417ec5887941a85209063233dcdc916
Baljinder Singh <manish-g@hcl.com>
2020-01-10 01:22:01 +0530

    webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,10 @@
-const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  module : {
-    rules: [
-      {
-        test   : /\.sass$/,
-        loader : 'postcss-loader',
-        options: {
-          ident  : 'postcss',
-          plugins: () => [
-            require('postcss-short')(),
-          ]
-        }
-      }
-    ]
-  },
   plugins: [
-   
+    new webpack.DefinePlugin({
+      'STABLE_FEATURE': JSON.stringify(true),
+      'EXPERIMENTAL_FEATURE': JSON.stringify(false)
+    })
   ]
 };
